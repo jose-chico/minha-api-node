24e13b08d0094e2acf8c622d9d111fcb3aa3fe29
jose-chico <franciscoclassname@gmail.com>
2025-09-10 12:28:38 -0300

    rotas

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { router } from "./routes/router";
 import cors from "cors";
+import path from "path";
 
 
 import dotenv from "dotenv"; 
@@ -11,6 +12,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(router);
 
 import { verifySMTP } from "./controllers/services/mailer/mailer";
